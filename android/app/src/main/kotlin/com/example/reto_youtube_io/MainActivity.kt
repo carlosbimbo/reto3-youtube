4f9555d35d03d39e163b6a524d3f70fbeac00ec6
cabs <carlosbars8@gmail.com>
2023-11-12 19:03:28 -0500

    Add reto 3 youtube config inicial 01

---
+++ android/app/src/main/kotlin/com/example/reto_youtube_io/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.reto_youtube_io
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/utils/constants.dart
@@ -0,0 +1,2 @@
+String pathProduction = "https://youtube.googleapis.com/youtube/v3";
+String apiKey = "AIzaSyA7S5KW9V1zCpYcxHFiff9kE51EWBNad_Y";
